3ecebf6d837169a4d4748d7ca36f10f780df7925
JustinConforti <jconforti@netsafesolutions.com>
2023-08-25 10:58:59 -0400

    mobile responsive and continuing work on navbar

--- my-react-app/src/components/navbar.js
+++ my-react-app/src/components/navbar.js
@@ -3,16 +3,27 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Catimg from '../images/cats_and_dogs.jpg';
+import whiteGlobe from '../images/whiteGlobe.png';
+import cartoonCloud from '../images/birdCloud.svg'
 
 function NavBar({weatherData}) {
+    console.log(weatherData.main.temp)
+    const temp = Math.round(weatherData.main.temp)
     return (
-    <div className ="navbarColor">
-    <Container className ="LeftSideNavBarContainer" fluid>
-        <Row>
-          <Col><img className ="dogcatimg" src={ Catimg } /></Col>
-          <Col className ="NavBarTitleText">Its Raining Cats</Col>
-          <Col className ="NavBarCityInfo">{weatherData.name} </Col>
+    <div className ="navBar">
+    <Container className="firstNavBarContainer" fluid>
+        <Row class="firstNavBarRow">
+          <Col><img class="" src={ Catimg } /></Col>
+          <Col><h3 class="">A Cat and Dog Business</h3></Col>
+          <Col className="whiteGlobe">{weatherData.name}<img src={ whiteGlobe } /></Col>
         </Row>
+    </Container>
+
+    <Container className="secondNavBarContainer">
+        <Row>
+            <Col className="secondNavBarContent"><img src = {cartoonCloud } /><span>{temp}°</span><span classname="secondNavBarCity"> {weatherData.name}, NC</span></Col>
+        </Row>
+
     </Container>
 
     </div>
